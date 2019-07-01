94fa7d5b01d1809c9cf725bcdea443c657d6710a
boblee <jipoer9@hotmail.com>
2019-07-01 17:35:23 +0800

    call apply bind实行

---
+++ call-apply-bind实现.js
@@ -0,0 +1,43 @@
+Function.prototype.callMe = function(){
+  var bindThis = arguments[0] || window;
+  var fn = this;
+  var args = []
+  for(var i=1;i<arguments.length;i++){
+    // 这里要push 这行字符串  而不是直接push 值
+    // 因为直接push值会导致一些问题
+    // 例如: push一个数组 [1,2,3]
+    // 在下面👇 eval调用时,进行字符串拼接,JS为了将数组转换为字符串 ，
+    // 会去调用数组的toString()方法,变为 '1,2,3' 就不是一个数组了，相当于是3个参数.
+    // 而push这行字符串，eval方法，运行代码会自动去arguments里获取值
+    args.push('arguments['+i+']');
+  }
+  bindThis.f = fn;
+  var result = eval('bindThis.f('+args+')')
+  delete bindThis.f
+  return result;
+}
+
+Function.prototype.applyMe = function () {
+  var bindThis = arguments[0] || window;
+  var fn = this;
+  var args = arguments[1]
+  bindThis.f = fn;
+  var result = eval('bindThis.f(' + args + ')')
+  delete bindThis.f
+  return result;
+}
+
+Function.prototype.bindMe = function(){
+  var fn = this;
+  var bindThis = arguments[0] || window
+  var args = []
+  for (var i = 1; i < arguments.length; i++) {
+    args.push(arguments[i]);
+  }
+  return function(){
+    for (var i = 0; i < arguments.length; i++) {
+      args.push(arguments[i]);
+    }
+    return fn.apply(bindThis,args)
+  }
+}
